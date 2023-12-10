7b708773198f124d0b0da39c27cbd02c181da587
Mehrzad Asri <mehrzad.asri@protonmail.ch>
2023-12-10 19:49:02 +0330

    Initial commit

---
+++ audio-plugin.js
@@ -0,0 +1,463 @@
+/**
+ * noVNC audio plugin
+ * A drop-in noVNC plugin for out-of-band audio playback
+ * 
+ * Copyright (C) 2023 Mehrzad Asri
+ * Licensed under MPL 2.0
+ */
+
+import NVUI from "/app/ui.js";
+
+// Helper class for using MediaSource with data segments
+class MediaSourcePlayer {
+    mediaSource;
+    sourceBuffer;
+
+    #directFeed = true; // First data is always fed directly
+    #dataQueue = [];
+
+    #attachedEl;
+
+    constructor(mime) {
+        this.mediaSource = new MediaSource();
+
+        this.mediaSource.addEventListener('sourceopen', () => {
+            // See https://developer.mozilla.org/en-US/docs/Web/Media/Formats/codecs_parameter for more
+            this.sourceBuffer = this.mediaSource.addSourceBuffer(mime);
+
+            // Playback media segments in the same order they are inserted in
+            this.sourceBuffer.mode = 'sequence';
+
+            this.sourceBuffer.addEventListener('updateend', () => {
+                // Stop here if for whatever reason the source buffer is still updating
+                if (this.sourceBuffer.updating) {
+                    return;
+                }
+
+                // Do a direct feed next time if queue is empty
+                if (this.#dataQueue.length == 0) {
+                    this.#directFeed = true;
+                    return;
+                }
+
+                // Get next data from queue and add it to the source buffer
+                const data = this.#dataQueue[0];
+
+                try {
+                    this.sourceBuffer.appendBuffer(data);
+                    this.#dataQueue.shift();
+                } catch (err) {
+                    // If quota full, drop half of the buffer
+                    // See https://developer.chrome.com/blog/quotaexceedederror
+                    if (err.name == 'QuotaExceededError') {
+                        console.log('SourceBuffer quota exceeded. Dropping half of the buffer.');
+
+                        const bufferStart = this.sourceBuffer.buffered.start(0);
+                        const bufferEnd = this.sourceBuffer.buffered.end(0);
+                        const removeEnd = bufferEnd - Math.max((bufferEnd - bufferStart) / 2, 1);
+
+                        this.sourceBuffer.remove(0, removeEnd);
+                        if (!this.sourceBuffer.updating) {
+                            this.sourceBuffer.appendBuffer(data);
+                            this.#dataQueue.shift();
+                        }
+                        return;
+                    }
+                    else {
+                        throw err;
+                    }
+                }
+            });
+        }, { once: true });
+    }
+
+    async attach(element) {
+        if (this.#attachedEl) {
+            throw new Error('Already attached to an element');
+        }
+
+        element.src = URL.createObjectURL(this.mediaSource);
+        element.addEventListener('play', MediaSourcePlayer.playEventHandler);
+        this.#attachedEl = element;
+
+        return new Promise((resolve) => {
+            this.mediaSource.addEventListener('sourceopen', () => {
+                resolve();
+            }, { once: true });
+        });
+    }
+
+    async detach() {
+        if (this.#attachedEl) {
+            this.#attachedEl.removeEventListener('play', MediaSourcePlayer.playEventHandler);
+
+            await this.#attachedEl.pause();
+            this.#attachedEl.removeAttribute('src');
+            this.#attachedEl.currentTime = 0;
+        }
+    }
+
+    feed(data) {
+        if (!this.#attachedEl) {
+            throw new Error('Not attached to any elements');
+        }
+        if (this.mediaSource.readyState != 'open') {
+            throw new Error(`Bad MediaSource state: ${this.mediaSource.readyState}`);
+        }
+
+        // Feed directly if direct feed is enabled otherwise queue data
+        if (this.#directFeed) {
+            this.sourceBuffer.appendBuffer(data);
+
+            // Disable direct feed if source buffer is updating
+            if (this.sourceBuffer.updating) {
+                this.#directFeed = false;
+            }
+        } else {
+            this.#dataQueue.push(data);
+        }
+    }
+
+    static playEventHandler(event) {
+        const audioEl = event.target;
+
+        // Make sure we're always playing the live edge of the stream
+        // Mostly necessary if some external entity decided to pause the media
+        if (audioEl.seekable.length > 0) {
+            audioEl.currentTime = audioEl.seekable.end(0);
+        }
+    }
+}
+
+// Helper functions for interacting with the NoVNC UI
+const NV = {
+    getMainSettingsList() {
+        return document.querySelector('#noVNC_settings ul');
+    },
+
+    addSubCategory(parent, label) {
+        const settingsItem = document.createElement('li');
+
+        const expanderDiv = document.createElement('div');
+        expanderDiv.classList.add('noVNC_expander');
+        expanderDiv.innerHTML = label;
+
+        expanderDiv.addEventListener('click', NVUI.toggleExpander);
+
+        const childDiv = document.createElement('div');
+
+        const listDiv = document.createElement('ul');
+        childDiv.appendChild(listDiv);
+
+        settingsItem.appendChild(expanderDiv);
+        settingsItem.appendChild(childDiv);
+
+        parent.appendChild(settingsItem);
+
+        return listDiv;
+    },
+
+    addInput(settingsList, label, name, defaultVal = null, type = 'text', title = null) {
+        const settingItem = document.createElement('li');
+
+        const settingLabel = document.createElement('label');
+
+        const settingInput = document.createElement('input');
+        settingInput.id = `noVNC_setting_${name}`;
+        settingInput.type = type;
+
+        if (title) {
+            settingLabel.title = title;
+            settingInput.title = title;
+        }
+
+        settingItem.appendChild(settingLabel);
+        if (type == 'checkbox') {
+            settingLabel.appendChild(settingInput);
+        } else {
+            settingLabel.htmlFor = settingInput.id;
+            settingItem.appendChild(settingInput);
+        }
+        settingLabel.appendChild(document.createTextNode(label));
+
+        settingInput.addEventListener('change', () => NVUI.saveSetting(name));
+
+        settingsList.appendChild(settingItem);
+
+        NVUI.initSetting(name, defaultVal);
+
+        return settingInput;
+    },
+
+    addDropdown(settingsList, label, name, values, defaultVal = null, title = null) {
+        const settingItem = document.createElement('li');
+
+        const settingLabel = document.createElement('label');
+        settingLabel.innerText = label;
+
+        const settingSelect = document.createElement('select');
+        settingSelect.id = `noVNC_setting_${name}`;
+        settingLabel.htmlFor = settingSelect.id;
+
+        if (title) {
+            settingLabel.title = title;
+            settingSelect.title = title;
+        }
+
+        settingItem.appendChild(settingLabel);
+        settingItem.appendChild(settingSelect);
+
+        settingSelect.addEventListener('change', () => NVUI.saveSetting(name));
+
+        for (const [name, val] of Object.entries(values)) {
+            const option = document.createElement('option');
+            option.text = name;
+            option.value = val;
+
+            settingSelect.appendChild(option);
+        }
+
+        settingsList.appendChild(settingItem);
+
+        NVUI.initSetting(name, defaultVal);
+
+        return settingSelect;
+    },
+
+    addLineBreak(settingsList) {
+        const settingItem = document.createElement('li');
+
+        const lineBreak = document.createElement('hr');
+        settingItem.appendChild(lineBreak);
+
+        settingsList.appendChild(settingItem);
+
+        return lineBreak;
+    },
+
+    observeState(state, callback, once = false) {
+        const doc = document.documentElement;
+
+        const observer = new MutationObserver(async () => {
+            if ((state == 'disconnected' && doc.classList.length == 0)
+                || doc.classList.contains(`noVNC_${state}`)) {
+                await callback(observer);
+
+                if (once) {
+                    observer.disconnect();
+                }
+            }
+        });
+        observer.observe(doc, { attributes: true, attributeFilter: ['class'] });
+    }
+};
+
+// Helper functions for the audio proxy
+const AudioProxy = {
+    handshake(socket, codec = 'opus', bitrate = 96000, sampleRate = 48000, secret = null) {
+        const textEnc = new TextEncoder();
+        const textDec = new TextDecoder();
+
+        let handshakeMsg = `CD:${codec}\nBR:${bitrate}\nSR:${sampleRate}\n\n`;
+        if (secret != null) {
+            handshakeMsg += `sec:${secret}`;
+        }
+        handshakeMsg += `\n`;
+        socket.send(textEnc.encode(handshakeMsg));
+
+        return new Promise((resolve) => {
+            socket.addEventListener('message', (msg) => {
+                const resp = textDec.decode(msg.data).trim();
+                if (resp == 'READY') {
+                    resolve();
+                } else if (resp.startsWith('ERR:')) {
+                    throw new Error(`Proxy error: ${resp.substring(4)}`);
+                } else {
+                    throw new Error('Protocol error');
+                }
+            }, { once: true });
+        });
+    }
+};
+
+const AudioPlugin = {
+    msp: null,
+    ws: null,
+    audioEl: null,
+
+    async onClickPlayHandler() {
+        try {
+            await this.audioEl.play();
+        } catch (err) {
+            if (err.name != 'AbortError') {
+                NVUI.showStatus(`Audio playback failed: ${err.message}`, 'error');
+            }
+            await this.stopAudio();
+        }
+    },
+
+    async startAudio() {
+        if (this.msp) {
+            return;
+        }
+
+        const codec = NVUI.getSetting('audio_codec');
+        const bitrate = NVUI.getSetting('audio_bitrate');
+        const samplerate = NVUI.getSetting('audio_samplerate');
+        let mime;
+        switch (codec) {
+            case 'opus':
+                mime = 'audio/webm; codecs="opus"';
+                break;
+            case 'aac':
+                mime = 'audio/mp4; codecs="mp4a.40.2"';
+                break;
+            default:
+                throw new Error(`Unsupported codec ${codec}`);
+        }
+
+        const wsSchema = (NVUI.getSetting('audio_encrypt')) ? 'wss://' : 'ws://';
+        const wsHost = NVUI.getSetting('audio_host');
+        const wsPort = NVUI.getSetting('audio_port');
+        const wsPath = NVUI.getSetting('audio_path');
+
+        this.ws = new WebSocket(`${wsSchema}${wsHost}:${wsPort}/${wsPath}`);
+        this.ws.binaryType = 'arraybuffer';
+
+        this.ws.addEventListener('error', async () => {
+            if (NVUI.connected) {
+                NVUI.showStatus('Audio WebSocket connection failed', 'error');
+            }
+            await this.stopAudio();
+        });
+        this.ws.addEventListener('close', async () => {
+            if (!this.msp) {
+                return;
+            }
+
+            if (NVUI.connected) {
+                NVUI.showStatus('Audio WebSocket connection closed', 'error');
+            }
+            await this.stopAudio();
+        });
+
+        this.ws.addEventListener('open', async () => {
+            try {
+                this.msp = new MediaSourcePlayer(mime);
+                await this.msp.attach(this.audioEl);
+            } catch (err) {
+                NVUI.showStatus(`MediaSource initialization failed: ${err.message}`);
+
+                await this.stopAudio();
+                return;
+            }
+
+            try {
+                await AudioProxy.handshake(this.ws, codec, bitrate, samplerate);
+            } catch (err) {
+                NVUI.showStatus(`Audio handshake failed: ${err.message}`, 'error');
+
+                await this.stopAudio();
+                return;
+            }
+
+            this.ws.addEventListener('message', async (msg) => {
+                try {
+                    this.msp.feed(msg.data);
+                } catch (err) {
+                    NVUI.showStatus(`Audio failure: ${err.message}`, 'error');
+
+                    await this.stopAudio();
+                    return;
+                }
+            });
+
+            // Start playing audio on user click
+            // Necessary because most if not all browsers prevent autoplay
+            document.body.addEventListener('click', async () => this.onClickPlayHandler(), { capture: true, once: true });
+        });
+    },
+
+    async stopAudio() {
+        if (this.msp) {
+            await this.msp.detach();
+            this.msp = null;
+        }
+
+        if (this.ws) {
+            this.ws.close();
+            this.ws = null;
+        }
+    },
+
+    initUi() {
+        // == Audio element ==
+        this.audioEl = document.createElement('audio');
+        this.audioEl.id = 'noVNC_audio';
+
+        document.body.appendChild(this.audioEl);
+
+        // == Audio settings menu ==
+        const settingsList = NV.getMainSettingsList();
+
+        NV.addLineBreak(settingsList);
+
+        const audioSettings = NV.addSubCategory(settingsList, 'Audio Plugin');
+        NV.addInput(audioSettings, 'Enabled', 'audio_enabled', false, 'checkbox', 'Enable audio plugin');
+
+        NV.addLineBreak(audioSettings);
+
+        NV.addDropdown(audioSettings, 'Codec:', 'audio_codec', {
+            'WebM/Opus': 'opus',
+            'MP4/AAC': 'aac'
+        }, MediaSource.isTypeSupported('audio/webm; codecs="opus"') ? 'opus' : 'aac', 'Audio codec');
+        NV.addDropdown(audioSettings, 'Bitrate:', 'audio_bitrate', {
+            '64kbps': '64000',
+            '96kbps': '96000',
+            '128kbps': '128000',
+            '192kbps': '192000'
+        }, '96000', 'Audio bitrate');
+        NV.addDropdown(audioSettings, 'Sample Rate:', 'audio_samplerate', {
+            '44.1kHz': '44100',
+            '48kHz': '48000',
+        }, '48000', 'Audio sample rate\nNote: Opus always outputs 48kHz');
+
+        NV.addLineBreak(audioSettings);
+
+        NV.addInput(audioSettings, 'Secret:', 'audio_secret', null, 'password', 'Optional connection secret\nNote: this does NOT provide encryption');
+
+        const audioWsSettings = NV.addSubCategory(audioSettings, 'WebSocket');
+        NV.addInput(audioWsSettings, 'Encrypt', 'audio_encrypt', NVUI.getSetting('encrypt'), 'checkbox',
+            'Use encrypted WebSocket connection');
+        NV.addInput(audioWsSettings, 'Host:', 'audio_host', NVUI.getSetting('host'), 'text',
+            'WebSocket host for audio proxy');
+        NV.addInput(audioWsSettings, "Port:", 'audio_port', NVUI.getSetting('port'), 'text',
+            'WebSocket port for audio proxy');
+        NV.addInput(audioWsSettings, "Path:", 'audio_path', 'websockify?token=audio', 'text',
+            'WebSocket path for audio proxy');
+    },
+
+    load() {
+        this.initUi();
+
+        // Start audio after VNC connection
+        NV.observeState('connected', async () => {
+            if (!NVUI.getSetting('audio_enabled')) {
+                return;
+            }
+
+            try {
+                await this.startAudio();
+            } catch (err) {
+                NVUI.showStatus(`Audio setup failed: ${err.message}`, 'error');
+            }
+        });
+
+        // Stop audio after VNC disconnection
+        NV.observeState('disconnected', async () => {
+            await this.stopAudio();
+        });
+    }
+};
+
+window.addEventListener('load', () => AudioPlugin.load());
---
+++ audio-proxy.sh
@@ -0,0 +1,226 @@
+#!/bin/sh
+# shellcheck shell=dash
+
+readonly SCRIPT="$0"
+
+readonly PULSE_PORT='4711'
+readonly PULSE_FORMAT='s16le'
+readonly PULSE_SAMPLE_RATE='48000'
+readonly PULSE_CHANNELS='2'
+
+readonly TCP_BIND='127.0.0.1'
+
+print_usage() {
+	echo "Usage: ${SCRIPT} [OPTION]..."
+	echo "Audio proxy meant to be put behind Websockify to provide audio to NoVNC audio plugin"
+	echo
+	echo 'Options:'
+	echo " -p <port>        : raw audio source port (default: ${PULSE_PORT})"
+	echo ' -l <port>        : listen for clients on specified TCP port'
+	echo " -b <host>        : bind TCP listener to specified host (default: ${TCP_BIND})"
+	echo ' -u <path>        : listen for clients on specified Unix socket path'
+	echo ' -s <path>        : secret file path (note: this does NOT provide any kind of encryption)'
+	echo " -f <format>      : raw audio source format (default: ${PULSE_FORMAT})"
+	echo " -r <sample rate> : raw audio source sample rate (default: ${PULSE_SAMPLE_RATE})"
+	echo " -c <channels>    : raw audio source channel count (default: ${PULSE_CHANNELS})"
+}
+
+error() {
+	echo "$1" >&2
+	exit 1
+}
+
+usage_error() {
+	echo "$1" >&2
+	print_usage >&2
+
+	exit 1
+}
+
+proto_ready() {
+	echo "READY"
+}
+
+proto_error() {
+	echo "ERR:$1"
+	exit 1
+}
+
+aac_proxy() {
+	local pulse_port="$1"
+	local pulse_format="$2"
+	local pulse_sample_rate="$3"
+	local pulse_channels="$4"
+
+	local bitrate="$5"
+	local sample_rate="$6"
+
+	proto_ready
+
+	exec gst-launch-1.0 -q mp4mux streamable=true fragment-duration=10 name=mux ! fdsink fd=1 \
+		tcpclientsrc port="${pulse_port}" ! queue ! rawaudioparse use-sink-caps=false format=pcm pcm-format="${pulse_format}" sample-rate="${pulse_sample_rate}" num-channels="${pulse_channels}" \
+		! audioconvert ! audioresample ! audio/x-raw, rate="${sample_rate}" ! fdkaacenc afterburner=true bitrate="${bitrate}" ! mux.audio_0
+}
+
+opus_proxy() {
+	local pulse_port="$1"
+	local pulse_format="$2"
+	local pulse_sample_rate="$3"
+	local pulse_channels="$4"
+
+	local bitrate="$5"
+
+	proto_ready
+
+	exec gst-launch-1.0 -q webmmux name=mux ! fdsink fd=1 \
+		tcpclientsrc port="${pulse_port}" ! rawaudioparse use-sink-caps=false format=pcm pcm-format="${pulse_format}" sample-rate="${pulse_sample_rate}" num-channels="${pulse_channels}" \
+		! audioconvert ! audioresample ! opusenc bitrate="${bitrate}" bitrate-type=0 complexity=4 frame-size=10 ! mux.audio_0
+}
+
+proxy() {
+	local pulse_port="$1"
+	local pulse_format="$2"
+	local pulse_sample_rate="$3"
+	local pulse_channels="$4"
+
+	local secret_file="$5"
+
+	# Default to Opus
+	local codec='opus'
+	local bitrate='96000'
+	local sample_rate='48000'
+
+	local secret
+
+	local line
+	while IFS= read -r line; do
+		if [ -z "${line}" ]; then
+			break
+		fi
+
+		case "${line}" in
+		*':'*) ;;
+		*)
+			proto_error 'bad handshake' ;;
+		esac
+		local opt
+		opt="$(echo "${line}" | cut -d ':' -f 1)"
+		local val
+		val="$(echo "${line}" | cut -d ':' -f 2-)"
+
+		case "${opt}" in
+		'CD')
+			codec="${val}" ;;
+		'BR')
+			bitrate="${val}" ;;
+		'SR')
+			sample_rate="${val}" ;;
+		'SEC')
+			secret="${val}" ;;
+		*)
+			proto_error "invalid option ${opt}" ;;
+		esac
+	done
+
+	if [ -n "${secret_file}" ]; then
+		if [ "${secret}" != "$(cat "${secret_file}")" ]; then
+			proto_error 'bad secret'
+		fi
+	fi
+
+	case "${codec}" in
+	'opus')
+		opus_proxy "${pulse_port}" "${pulse_format}" "${pulse_sample_rate}" "${pulse_channels}" "${bitrate}" "${sample_rate}" ;;
+	'aac')
+		aac_proxy "${pulse_port}" "${pulse_format}" "${pulse_sample_rate}" "${pulse_channels}" "${bitrate}" ;;
+	*)
+		proto_error "invalid codec ${codec}";
+	esac
+}
+
+server() {
+	local pulse_port="${PULSE_PORT}"
+	local pulse_format="${PULSE_FORMAT}"
+	local pulse_sample_rate="${PULSE_SAMPLE_RATE}"
+	local pulse_channels="${PULSE_CHANNELS}"
+
+	local secret_file
+
+	local tcp_port
+	local tcp_bind="${TCP_BIND}"
+
+	local unix_socket
+
+	while getopts 'p:l:b:u:s:f:r:c:h' opt; do
+		case "${opt}" in
+		'p')
+			pulse_port="${OPTARG}"
+		;;
+		'l')
+			tcp_port="${OPTARG}"
+		;;
+		'b')
+			tcp_bind="${OPTARG}"
+		;;
+		'u')
+			unix_socket="${OPTARG}"
+		;;
+		's')
+			secret_file="${OPTARG}"
+		;;
+		'f')
+			pulse_format="${OPTARG}"
+		;;
+		'r')
+			pulse_sample_rate="${OPTARG}"
+		;;
+		'c')
+			pulse_channels="${OPTARG}"
+		;;
+		'h')
+			print_usage
+			exit 0
+		;;
+		*)
+			print_usage
+			exit 1
+		;;
+		esac
+	done
+	shift $((OPTIND - 1))
+
+	if [ -z "${tcp_port}" ] && [ -z "${unix_socket}" ]; then
+		usage_error 'At least a listening TCP port or Unix socket is required'
+	fi
+	if [ -n "${tcp_port}" ] && [ -n "${unix_socket}" ]; then
+		usage_error '-u and -l options are mutually exclusive'
+	fi
+
+	echo "Raw source port: ${pulse_port}"
+	echo "Raw source format: ${pulse_format}"
+	echo "Raw source sample rate: ${pulse_sample_rate}"
+	echo "Raw source channels: ${pulse_channels}"
+
+	local proxy_cmd="${SCRIPT} proxy ${pulse_port} ${pulse_format} ${pulse_sample_rate} ${pulse_channels} ${secret_file}"
+	if [ -n "${tcp_port}" ]; then
+		echo "Server listening on ${tcp_bind}:${tcp_port}"
+		exec socat tcp-listen:"${tcp_port}",bind="${tcp_bind}",nodelay,reuseaddr,fork exec:"${proxy_cmd}",nofork
+	elif [ -n "${unix_socket}" ]; then
+		echo "Server listening on '${unix_socket}'"
+		exec socat unix-listen:"${unix_socket}",fork exec:"${proxy_cmd}",nofork
+	fi
+}
+
+if ! command -v socat  >/dev/null 2>&1; then
+	error 'socat not found. Is it installed?'
+fi
+if ! command -v gst-launch-1.0 >/dev/null 2>&1; then
+	error 'GStreamer (gst-launch-1.0) not found. Is it installed?'
+fi
+
+if [ "$1" = 'proxy' ]; then
+	shift
+	proxy "$@"
+else
+	server "$@"
+fi
